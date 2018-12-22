2d85e0545270e1a37dc6980bf605e03fd05519ea
Brandon Zupan <brandonzforty2@gmail.com>
2018-12-21 21:17:08 -0600

    Added color output as text

--- colorDetection.py
+++ colorDetection.py
@@ -8,7 +8,7 @@ aquireImage()   #Downloads the image file
 #time.sleep(5)
 
 try:
-        path = "tower.jpg"
+        path = "fullorange.jpg"
         im = Image.open(path)
 
 #If there is no file, print error and exit
@@ -20,6 +20,8 @@ baseBox = (718,217,731,358)         #Bounding box for the base of the tower
 baseRegion = im.crop(baseBox)   
 baseX = 731 - 718
 baseY = 358 - 217
+
+baseRegion.show()
 
 baseColor = (42,42,42)
 R = 0
@@ -47,7 +49,7 @@ topRegion = im.crop(topBox)
 topX = 33
 topY = 40
 
-#topRegion.show()
+topRegion.show()
 
 topColor = (42,42,42)
 
@@ -72,11 +74,12 @@ B = int(B/totalPix)
 topColor = (R,G,B)
 
 #Prints outputs to console
-print("Color of tower base")
-print(baseColor)
-print("\n")
-print("Color of tower top")
-print(topColor)
+
+#print("Color of tower base")
+#print(baseColor)
+#print("\n")
+#print("Color of tower top")
+#print(topColor)
 
 #Write outputs to a file with base being first and top being second
 f = open("data.txt", "w")
@@ -93,9 +96,69 @@ def writeData(colorTuple):
                 else:
                         f.write("\n")
 
-writeData(baseColor)
-writeData(topColor)
+#writeData(baseColor)
+#writeData(topColor)
 
+#Figure out the actual color of the tower
+
+if (100 < baseColor[0] < 200):
+        if (50 < baseColor[1] < 150):
+                if (50 < baseColor[2] < 150):
+                        baseColorName = "Orange"
+                        baseColorNumber = "0"
+
+elif (175 < baseColor[0] < 255):
+        if (175 < baseColor[1] < 255):
+                if (175 < baseColor[2] < 255):
+                        baseColorName = "White"
+                        baseColorNumber = "1"
+
+elif (0 < baseColor[0] < 100):
+        if (0 < baseColor[1] < 100):
+                if (0 < baseColor[2] < 100):
+                        baseColorName = "Dark"
+                        baseColorNumber = "2"
+
+else:
+        baseColorName = "Unknown"
+        baseColorNumber = "-1"
+
+if (150 < topColor[0] < 255):
+        if (100 < topColor[1] < 200):
+                if (50 < topColor[2] < 150):
+                        topColorName = "Orange"
+                        topColorNumber = "0"
+
+        elif (200 < topColor[1] < 255):
+                topColorName = "White"
+                topColorNumber = "1"
+
+elif (0 < topColor[0] < 100):
+        topColorName = "Dark"
+        topColorNumber = "2"
+
+else:
+        topColorName = "Unknown"
+        topColorNumber = "-1"
+
+#print("The tower is " + baseColorName + " today!")
+
+#Output the color of the tower to console and a file
+if (topColorName == "White"):
+        print("The tower is white today!")
+        f.write("1,1")
+
+if (topColorName == "Orange" and baseColorName == "White"):
+        print("The tower is orange and white today!")
+        f.write("0,1")
+
+if (baseColorName == "Orange"):
+        print("The tower is orange today!")
+        f.write("0,0")
+
+if (baseColorName == "Dark"):
+        print("The tower is dark today")
+        f.write("2,2")
 
 ####################
 ##Expected Outputs##
@@ -110,8 +173,13 @@ writeData(topColor)
 #Dark Base:     (57,47,50)
 #Dark Top:      (56,44,49)
 
+#Tower can be:
+        #White
+        #White and Orange
+        #Orange
+        #Dark
+
 #What to do next:
-    #Add color detection to output *Tower is Orange today*
     #Yeet loud and proud
     #Turn into a twitter bot that uploads a cropped image of the tower and says the state
     #Implement into the discord server somehow (MUCH LATER LOL)
