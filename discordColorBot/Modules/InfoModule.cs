d247f3191fa8b7835fd54e016948e2d3fd65d811
Brandon Zupan <brandonzforty2@gmail.com>
2018-12-26 14:24:59 -0600

    Reprogrammed bot

---
+++ discordColorBot/Modules/InfoModule.cs
@@ -0,0 +1,92 @@
+﻿using Discord.Commands;
+using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace discordColorBot.Modules
+{
+    public class Info : ModuleBase
+    {
+        // ~say hello -> hello
+        [Command("say"), Summary("Echos a message.")]
+        public async Task Say([Remainder, Summary("The text to echo")] string echo)
+        {
+            // ReplyAsync is a method on ModuleBase
+            await ReplyAsync(echo);
+        }
+
+        [Command("ping")]
+        public async Task Ping([Summary("Says Pong!")] string yeet = null)
+        {
+            await Context.Channel.SendMessageAsync("Pong!");
+        }
+
+        [Command("updateicon")]
+        public async Task UpdateIcon([Summary("Updates the tower icon based on the tower's color")] string yeet = null)
+        {
+            var user = Context.User as SocketGuildUser;
+            var permission = (user as IGuildUser).GuildPermissions;
+            //Add ability to check user role
+            {
+
+            }
+        }
+
+        [Command("towercolor")]
+        public async Task TowerColor([Summary("Provides current color of tower")] string yeet = null)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = @"py ..\colorDetection.py";
+            process.StartInfo = startInfo;
+            process.Start();
+
+            string messageText = "";
+
+            string colorData = File.ReadAllText(@"C: \Users\brand\Documents\Programming\utTowerColor\data.txt");
+            //await message.Channel.SendMessageAsync("Command Recieved");
+
+            if (colorData == "0,0")
+            {
+                messageText = "The tower is Orange today!";
+            }
+
+            else if (colorData == "0,1")
+            {
+                messageText = "The tower is orange and white today!";
+            }
+
+            else if (colorData == "1,1")
+            {
+                messageText = "The tower is white today!";
+            }
+
+            else if (colorData == "2,2")
+            {
+                messageText = "The tower is dark today";
+            }
+
+            else if (colorData == "3,3")
+            {
+                messageText = "The tower is not lit yet";
+            }
+
+            else
+            {
+                messageText = "Sorry, I do not know what color the tower is";
+            }
+
+            //await message.Channel.SendMessageAsync(messageText);
+            await Context.Channel.SendFileAsync(@"C: \Users\brand\Documents\Programming\utTowerColor\out.jpg", messageText);
+        }
+
+        
+    }
+}
+
+
+
+
--- discordColorBot/Program.cs
+++ discordColorBot/Program.cs
@@ -1,111 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 using Discord;
 using Discord.WebSocket;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
 using System.IO;
 
 namespace discordColorBot
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
-            => new Program().MainAsync().GetAwaiter().GetResult();
+        private CommandService commands;
+        private DiscordSocketClient client;
+        private IServiceProvider services;
 
-        public async Task MainAsync()
+        static void Main(string[] args) => new Program().Start().GetAwaiter().GetResult();
+
+        public async Task Start()
         {
-            var client = new DiscordSocketClient();
-
-            client.Log += Log;
-            client.MessageReceived += MessageReceived;
+            client = new DiscordSocketClient();
+            commands = new CommandService();
 
             string token = File.ReadAllText(@"C: \Users\brand\Documents\Programming\utTowerColor\discordColorBot\apikey.txt");
+
+            services = new ServiceCollection()
+                    .BuildServiceProvider();
+
+            await InstallCommands();
+
             await client.LoginAsync(TokenType.Bot, token);
             await client.StartAsync();
 
-            
-
-            
-
-
-        async Task MessageReceived(SocketMessage message)
-            {
-                string messageText;
-                
-                if (message.Content == "!towercolor")
-                {
-
-
-                    //Run the python script without starting a CMD instance
-                    System.Diagnostics.Process process = new System.Diagnostics.Process();
-                    System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-                    startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-                    startInfo.FileName = "cmd.exe";
-                    startInfo.Arguments = @"py ..\colorDetection.py";
-                    process.StartInfo = startInfo;
-                    process.Start();
-
-                    string colorData = File.ReadAllText(@"C: \Users\brand\Documents\Programming\utTowerColor\data.txt");
-                    //await message.Channel.SendMessageAsync("Command Recieved");
-
-                    if (colorData == "0,0")
-                    {
-                        messageText = "The tower is Orange today!";
-                    }
-
-                    else if (colorData == "0,1")
-                    {
-                        messageText = "The tower is orange and white today!";
-                    }
-
-                    else if (colorData == "1,1")
-                    {
-                        messageText = "The tower is white today!";
-                    }
-
-                    else if (colorData == "2,2")
-                    {
-                        messageText = "The tower is dark today";
-                    }
-
-                    else if (colorData == "3,3")
-                    {
-                        messageText = "The tower is not lit yet";
-                    }
-
-                    else
-                    {
-                        messageText = "Sorry, I do not know what color the tower is";
-                    }
-
-                    //await message.Channel.SendMessageAsync(messageText);
-                    await message.Channel.SendFileAsync(@"C: \Users\brand\Documents\Programming\utTowerColor\out.jpg", messageText);
-                } 
-
-                if (message.Content == "!updateicon")
-                {
-                    var user = message.User as SocketGuildUser;
-                    var role = (user as IGuildUser).Guild.Roles.FirstOrDefault(x => x.Name == "Founder");
-                    if (!userName.Roles.Contains(role))
-                    {
-
-                    }
-                }
-                
-
-            }
-
-
-            //I think this goes last
             await Task.Delay(-1);
         }
 
-        private Task Log(LogMessage msg)
+        public async Task InstallCommands()
         {
-            Console.WriteLine(msg.ToString());
-            return Task.CompletedTask;
+            // Hook the MessageReceived Event into our Command Handler
+            client.MessageReceived += HandleCommand;
+            // Discover all of the commands in this assembly and load them.
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly());
         }
+
+        public async Task HandleCommand(SocketMessage messageParam)
+        {
+            // Don't process the command if it was a System Message
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+            // Create a number to track where the prefix ends and the command begins
+            int argPos = 0;
+            // Determine if the message is a command, based on if it starts with '!' or a mention prefix
+            if (!(message.HasCharPrefix('~', ref argPos) || message.HasMentionPrefix(client.CurrentUser, ref argPos))) return;
+            // Create a Command Context
+            var context = new CommandContext(client, message);
+            // Execute the command. (result does not indicate a return value, 
+            // rather an object stating if the command executed successfully)
+            //This command was var result = await commands.ExecuteAsync(context, argPos, service); but i deleted service and it worked
+            var result = await commands.ExecuteAsync(context, argPos);
+            if (!result.IsSuccess)
+                await context.Channel.SendMessageAsync(result.ErrorReason);
+        }
+
     }
 }
